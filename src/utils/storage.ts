9c289c785071668365a741267bafe3b2ca7fc08b
WisdomPandovic <wisdompandovic@gmail.com>
2024-08-28 14:45:11 +0100

    oma

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,11 +1,27 @@
-import { Product } from '../types/index'; 
+// import { Product } from '../types/index'; 
+
+// export const getProducts = (): Product[] => {
+//     const products = localStorage.getItem('products');
+//     return products ? JSON.parse(products) : [];
+//   };
+  
+//   export const saveProducts = (products: Product[]): void => {
+//     localStorage.setItem('products', JSON.stringify(products));
+//   };
+  
+
+import { Product } from '../types/index';
 
 export const getProducts = (): Product[] => {
-    const products = localStorage.getItem('products');
-    return products ? JSON.parse(products) : [];
-  };
-  
-  export const saveProducts = (products: Product[]): void => {
-    localStorage.setItem('products', JSON.stringify(products));
-  };
-  
+    if (typeof window !== 'undefined') {
+        const products = localStorage.getItem('products');
+        return products ? JSON.parse(products) : [];
+    }
+    return []; // Return an empty array if localStorage is not available (e.g., server-side)
+};
+
+export const saveProducts = (products: Product[]): void => {
+    if (typeof window !== 'undefined') {
+        localStorage.setItem('products', JSON.stringify(products));
+    }
+};
